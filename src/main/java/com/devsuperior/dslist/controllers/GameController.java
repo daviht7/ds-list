f5a7859d5a9f465a99dd2bbfab7a673af246f967
Davi Holanda <daviht7@gmail.com>
2023-05-17 12:33:29 -0300

    get game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dtos.GameDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,11 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<GameMinDTO>> findAll() {
         return ResponseEntity.ok(gameService.findAll());
+    }
+
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameDTO> findById(@PathVariable("gameId") Long gameId) {
+        return ResponseEntity.ok(gameService.findById(gameId));
     }
 
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dtos.GameDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +19,12 @@ public class GameService {
 
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true) //não bloqueia banco de dados, somente consulta
+    public GameDTO findById(Long id) {
+        var game = gameRepository.findById(id).get();
+        return new GameDTO(game);
     }
 
 }
