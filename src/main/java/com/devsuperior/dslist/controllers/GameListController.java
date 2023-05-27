36f7b554525848c3216d0ed6f44856b5e24c5f7b
Davi Holanda <daviht7@gmail.com>
2023-05-27 19:21:21 -0300

    move game on list

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dtos.GameListDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
+import com.devsuperior.dslist.dtos.ReplacementDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,12 @@ public class GameListController {
     public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable("listId") Long listId) {
         return ResponseEntity.ok(gameService.findByList(listId));
     }
+
+    @PostMapping("/{listId}/replacement")
+    public ResponseEntity<?> move(@PathVariable("listId") Long listId, @RequestBody ReplacementDTO replacementDTO) {
+        gameListService.move(listId, replacementDTO.getSourceIndex(), replacementDTO.getSourceIndex();
+        return ResponseEntity.ok().build();
+    }
+
 
 }
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dtos.GameListDTO;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +12,32 @@ public class GameListService {
 
     private final GameListRepository gameListRepository;
 
-    public GameListService(GameListRepository gameListRepository) {
+    private final GameRepository gameRepository;
+
+    public GameListService(GameListRepository gameListRepository,GameRepository gameRepository) {
         this.gameListRepository = gameListRepository;
+        this.gameRepository = gameRepository;
     }
 
     public List<GameListDTO> findAll() {
         return gameListRepository.findAll().stream().map(GameListDTO::new).toList();
+    }
+
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        var gamesList = gameRepository.searchByList(listId);
+
+        var obj = gamesList.remove(sourceIndex);
+        gamesList.add(destinationIndex,obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId,gamesList.get(i).getId(),i);
+            
+        }
+
     }
 
 }
